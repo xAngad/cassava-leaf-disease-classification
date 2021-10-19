b3eb44925d92796e314bf98f4963cf859fff0a4a
Dorian Jimenez <xinhua49@g.ucla.edu>
2021-10-18 19:18:30 -0700

    edited StartingDataset.py

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -1,4 +1,9 @@
 import torch
+import numpy as np
+from PIL import Image
+import pandas as pd
+import os
+
 
 
 class StartingDataset(torch.utils.data.Dataset):
@@ -6,14 +11,36 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, csv_file, image_directory):
+
+        #image_directory = "../cassava-leaf-disease-classification/train_images"
+        #csv_file = "../"
+        
+        self.csv_file = pd.read_csv(csv_file)
+        self.image_directory = image_directory
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        filename = self.csv_file.iloc[index][0]
+        image_path = self.image_directory + '/' + filename
+        im = Image.open(image_path, 'r')
+        im = im.resize((224, 224))
+        im = np.array(im)
+        im = torch.from_numpy(im)
 
-        return inputs, label
+        im = im / 255
+        
+        label = self.csv_file.iloc[index][1]
+
+        print(im.size())
+
+        return im, label
 
     def __len__(self):
-        return 10000
+        return self.csv_file.shape[0]
+
+
+print("Hello world")
+train_dataset = StartingDataset('../cassava-leaf-disease-classification/train.csv', '../cassava-leaf-disease-classification/train_images')
+inp, lab = train_dataset[261]
+print(inp)
+print(lab)
